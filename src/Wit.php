ed02e463c08fbdcb4e28492e401b77af02f16969
Gustavo Manolo <gustavomanolo@gmail.com>
2016-07-06 19:10:46 -0500

    send messages

--- src/Wit.php
+++ src/Wit.php
@@ -1,5 +1,7 @@
 <?php
 	namespace Gustavomanolo\WitPhp;
+	use GuzzleHttp\Client as GuzzleClient;
+    use GuzzleHttp\RequestOptions;
 
 	class Wit{
 
@@ -14,6 +16,11 @@
 		const API_VERSION = '20160526';
 
         /**
+         * Default connection timeout
+         */
+        const CONNECTION_TIMEOUT = 5;
+
+        /**
          * @var string Wit.ai app token (found under Settings in the Wit console)
          */
         private $accessToken;
@@ -23,16 +30,58 @@
          */
         private $apiVersion;
 
+		/**
+		 * @var object (instance of GuzzleHttp\Client => http://docs.guzzlephp.org/en/latest/ )
+		 */
+        private $guzzleClient;
+
         /**
          * Wit constructor.
          * @param $accessToken
          * @param $apiVersion
          */
-        public function __construct($accessToken, $apiVersion = self::DEFAULT_API_VERSION)
+        public function __construct($accessToken, $apiVersion = self::API_VERSION)
 		{
 			$this->accessToken = $accessToken;
 			$this->apiVersion = $apiVersion;
+			$this->guzzleClient = new GuzzleClient([
+                'base_uri' => self::API_BASE_URI,
+                'timeout' => self::CONNECTION_TIMEOUT
+            ]);
 		}
+
+
+		public function message($message, $extraParams=[])
+		{
+            $query = [
+                'q' => $message
+            ];
+
+            //-> Remove key "q" if exist in $extraParams
+            if( array_key_exists('q', $extraParams) )
+            {
+                unset( $extraParams['q'] );
+            }
+
+            //-> Merge array for GET parameters
+            $queryParams = array_merge($query, $extraParams);
+
+            $options =  [
+                RequestOptions::QUERY => $queryParams,
+                RequestOptions::HEADERS => [
+                    'Authorization' => 'Bearer '.$this->accessToken,
+                ]
+            ];
+            $response = $this->guzzleClient->request('GET', 'message', $options);
+
+
+            // Get the decoded body
+            return json_decode((string) $response->getBody(), true);
+
+            //$body = $response->getBody();
+            //echo $body;
+		}
+
 
 
 		/**
--- tests/test.php
+++ tests/test.php
@@ -5,3 +5,8 @@
     use Gustavomanolo\WitPhp;
 
     $wit = new WitPhp\Wit('VDHFDYLOZ3HJTW6NN6SCSZAWNBA4AHZM');
+
+
+    $response = $wit->message("¿Cuál será el clima en la ciudad de México?");
+    
+    //print_r( $response['entities'] );
